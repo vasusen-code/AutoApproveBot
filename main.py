d31480b37053bfbe58d146417d5926de415f7e4c
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-28 23:29:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from decouple import config
 
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None) #pyro session
 AUTH = config("AUTH", default=None, cast=int)
 

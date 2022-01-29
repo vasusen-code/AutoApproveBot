09a16a29d9d5b7cf6f73cf1c83b628fe415a8873
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-29 11:27:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from decouple import config
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None) 
-AUTH = config("AUTH", default=None, cast=int)
 CHAT = config("CHAT", default=None, cast=int)
 
 import os, asyncio, logging
@@ -31,9 +30,9 @@ async def approve(c: Client, m: ChatJoinRequest):
         await asyncio.sleep(e.x + 2)
         await c.approve_chat_join_request(m.chat.id, m.from_user.id)
 
-@bot_client.on_message(filters.user(AUTH))
+@bot_client.on_message(filters.private & filters.incoming)
 async def well_yes(c, m):
-    await m.reply("I am alive!")
+    await m.reply("I am just a bot, i can't talk to you!")
 
 async def run_bot_():
     await bot_client.start()
